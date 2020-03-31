92f83b58c7233edca3aa28cd91e65a4d77623a28
Krista Simmons <kristasimmons.design@gmail.com>
2020-03-31 09:00:17 -0700

    Set up initial event listeners

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-//create IIFE that will return an object. Use IIFE so no other code can overwrite our data. For the outside, we do not have access to the inner scope (eg. "x" or "add")
+// Budget Controller
 var budgetController = (function(){
 
-    var x = 23;
-    // in the colsone: budgetController.x -> undefined
-
-    var add = function(a) {
-        return x + a;
-    }
 
 
     return {
         //add method. this inner function will always has access to variables and parameters of it's outer function, even if the outer function has already returned. This all works because of closures. var x and var add are in the closure, therefore they will not return in the console.
-        publicTest: function(b) {
-            return add(b);
-        }
+
     }
     // in the console: budgetController.publicTest(5)-> 28
 
 })();
 
 
+
+
+
+// UI Controller
 var UIController = (function(){
 
 })();
@@ -27,18 +23,38 @@ var UIController = (function(){
 
 
 
+
+
+//Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
-    //we will pass other 2 modules as arguments to the controller in order to connect the above controllers
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+        //1. Get the field input data
 
-    return {
-        //adding another function is the only way we can access the "z" variable which is stored in the closure
-        anotherPublic: function() {
-            console.log(z);
-        }
+        //2. Add the item to the budget controller
 
-        //console : controller.anotherPublic() -> 28
+
+        //3. Add the new items to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+        console.log('It works.');
     }
 
+    //we will pass other 2 modules as arguments to the controller in order to connect the above controllers
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    //Set up key press event (in addition to a button click)
+    document.addEventListener('keypress', function(event) {
+        // console.log(event);
+        //use 'which' for old browser compatibility
+        if(event.keycode === 13 || event.which == 13) {
+            // console.log('Enter was pressed');
+            //call ctrlAddItem function
+            ctrlAddItem();
+        }
+    })
+
 })(budgetController, UIController);
+
